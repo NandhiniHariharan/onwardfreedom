e2f59cd98346d6e869f34b34a6402e53c67329f3
NandhiniHariharan <hariharannandi@gmail.com>
2024-09-09 18:49:07 -0700

    change

---
+++ Day19/Person.java
@@ -0,0 +1,27 @@
+package Day19;
+
+public class Person {
+    private String name;
+    private  String gender;
+    private int age;
+    public Person(String name, String gender, int age){
+     this.name=name;
+     this.gender=gender;
+     this.age=age;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getGender(){
+        return gender;
+    }
+    public int getAge(){
+        return age;
+    }
+    public static void main(String[] args) {
+        Person person1=new Person("Yashika","Female",21);
+        System.out.println(person1.getName());
+        System.out.println(person1.getGender());
+        System.out.println(person1.getAge());
+    }
+}
