d2670db72670d13ed5d9c15d1d2cc05142ad9de3
NandhiniHariharan <hariharannandi@gmail.com>
2024-10-18 22:21:14 -0700

    palindorme Number

---
+++ Day25/DailyPracticeDay1/PalindromeNumber.java
@@ -0,0 +1,14 @@
+package Day25.DailyPracticeDay1;
+import java.util.Scanner;
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sca=new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int number=sca.nextInt();
+        sca.close();
+        
+        for(int i=0;i<number.length;i++){
+        
+        }
+    }
+}
