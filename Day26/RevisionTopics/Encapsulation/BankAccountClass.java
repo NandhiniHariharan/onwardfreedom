c18c0d82b54576e390071f1effa757835a7b669a
NandhiniHariharan <hariharannandi@gmail.com>
2024-10-26 13:18:00 -0700

    Encapsulation Practice

---
+++ Day26/RevisionTopics/Encapsulation/BankAccountClass.java
@@ -0,0 +1,48 @@
+package Day26.RevisionTopics.Encapsulation;
+
+public class BankAccountClass {
+    private String accountNumber;
+    private String holderName;
+    private double balance;
+    public BankAccountClass(String accountNumber,String holdername, double initialBalnace ){
+        this.accountNumber=accountNumber;
+        this.holderName=holdername;
+        this.balance=initialBalnace;
+    }
+    public void deposit(double amount){
+        if(amount>0){
+            balance+=amount;
+        }else{
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+    public void withdrawal(double amount){
+        if(amount>0 && amount<=balance){
+            balance-=amount;
+        }else if(amount>balance){
+            System.out.println("Withdrawal amount exceeds balance.");
+        }else{
+            System.out.println("Deposit amount must be positive.");
+        }
+    }
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+    public String getHolderName(){
+        return holderName;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public static void main(String[] args) {
+        BankAccountClass bank=new BankAccountClass("ABC12437", "Johny", 1000.0);
+        System.out.println("Account Number: " + bank.getAccountNumber());
+        System.out.println("Holder Name: " + bank.getHolderName());
+        System.out.println("Initial Balance: $" + bank.getBalance());
+        bank.deposit(500.0);
+        System.out.println("Balance after deposit: "+bank.getBalance());
+        bank.withdrawal(200.0);
+        System.out.println("Balance after Withdrawal: "+bank.getBalance());
+        bank.withdrawal(2000.0);
+    }
+}
---
+++ Day26/RevisionTopics/Encapsulation/StudentDetails.java
@@ -0,0 +1,46 @@
+package Day26.RevisionTopics.Encapsulation;
+
+public class StudentDetails {
+    private int StudentID;
+    private String StudentName;
+    private int[] Grades;
+    public void setStudentID(int StudentID){
+        this.StudentID=StudentID;
+    }
+    public int getStudentID(){
+    return StudentID;
+    }
+    public void setStudentName(String StudentName){
+        this.StudentName=StudentName;
+    }
+    public String getStudentName(){
+        return StudentName;
+    }
+    public void setGrades(int[] Grades){
+        this.Grades=Grades;
+    }
+    public int[] getGrades(){
+        return Grades;
+    }
+    public double calculateAverge(){
+        if(Grades == null || Grades.length ==0 ){
+            return 0;
+        }
+        int sum=0;
+        for(int grades:Grades){
+            sum+=grades;
+        }
+        return (double) sum/Grades.length;
+    }
+
+    public static void main(String[] args) {
+        int[] Grades={85, 90, 78, 92};
+        StudentDetails student=new StudentDetails();
+        student.setStudentID(12356);
+        student.setStudentName("Soniya");
+        student.setGrades(Grades);
+        System.out.println(student.getStudentID());
+        System.out.println(student.getStudentName());
+        System.out.println(student.calculateAverge());
+    }
+}
