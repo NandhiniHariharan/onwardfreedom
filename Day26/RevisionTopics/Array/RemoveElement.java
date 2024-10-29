ac39e44a372cd42104c5b5a50dab220e88e71fa9
NandhiniHariharan <hariharannandi@gmail.com>
2024-10-28 22:46:09 -0700

    Remove Element in Array

---
+++ Day26/RevisionTopics/Array/RemoveElement.java
@@ -0,0 +1,21 @@
+package Day26.RevisionTopics.Array;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+        int[] nums={3,2,2,3};
+        int k=0;
+        int val=3;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        System.out.println("K= "+k);
+         System.out.print("Modified array: [");
+         for (int i = 0; i < k; i++) {
+             System.out.print(nums[i] + ",");
+         }
+         System.out.println("]");
+    }
+}
