e91f3b37ea2e12badc7a5a60ceea6ed8aa1f3714
NandhiniHariharan <hariharannandi@gmail.com>
2024-11-05 20:57:22 -0800

    Conatins Duplicate (Sorting)

---
+++ Day26/RevisionTopics/Sorting/ContainsDuplicate.java
@@ -0,0 +1,18 @@
+package Day26.RevisionTopics.Sorting;
+import java.util.HashSet;
+public class ContainsDuplicate {
+    public static boolean duplicateFinding(int[] nums){
+        HashSet<Integer> seen=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(seen.contains(nums[i])){
+                return true;
+            }
+            seen.add(nums[i]);
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        int[]nums={1,2,3,4};
+        System.out.println(duplicateFinding(nums));
+    }
+}
