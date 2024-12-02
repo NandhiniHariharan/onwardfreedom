92f75c3b041687e405c84e0526521c9b75d220de
NandhiniHariharan <hariharannandi@gmail.com>
2024-12-02 12:55:27 -0800

    Minimum Cuts to Divide a Circle

---
+++ Day28/Dec2024WeakTask/MinimumCutstoDivideaCircle.java
@@ -0,0 +1,21 @@
+package Day28.Dec2024WeakTask;
+
+public class MinimumCutstoDivideaCircle {
+    public int dividingCricle(int n){
+        if (n<=1){
+            return 0;
+        }
+        if(n%2==1){
+            return n;
+        }
+        else{
+            n=n/2;
+        }
+        return n;
+    }
+    public static void main(String[] args) {
+        int n=0;
+        MinimumCutstoDivideaCircle mini=new MinimumCutstoDivideaCircle();
+        System.out.println(mini.dividingCricle(n));
+    }
+}
