6f7e32a928b228367b3c07884f883649b5c5680d
NandhiniHariharan <hariharannandi@gmail.com>
2024-12-17 14:23:18 -0800

    Two Sum

--- Day25/LeetCode/TwoSum.java
+++ Day25/LeetCode/TwoSum.java
@@ -1,21 +1,25 @@
 package Day25.LeetCode;
 
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class TwoSum {
-
-    public static <T extends Number> void addition(T number){
-        double result= number.doubleValue()*2;
-        System.out.println(result);
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i]; 
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+           return new int[] {};
     }
-
     public static void main(String[] args) {
-
-        addition(10);
-
-        addition(5.5f);
-
+        int[] nums={2,7,10,22};
+        int target=9;
+        TwoSum sum=new TwoSum();
+        int[] result=sum.twoSum(nums,target);
+        System.out.println(Arrays.toString(result));
     }
-
 }
