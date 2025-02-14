a87cc3f9fd825bee030da3f7e6c8d9be07975104
NandhiniHariharan <hariharannandi@gmail.com>
2025-02-13 23:12:04 -0800

    Products of Array Except Self

---
+++ Day30/Feb2025WeekTask/Neetcode/ProductofArrayExceptSelf.java
@@ -0,0 +1,28 @@
+package Feb2025WeekTask.Neetcode;
+
+public class ProductofArrayExceptSelf {
+    public int[] prductofarray(int[] nums){
+        int[] ans=new int[nums.length];
+        int prefix=1;
+        int suffix=1;
+        for(int i=0;i<nums.length;i++){
+            ans[i]=prefix;
+            prefix=prefix*nums[i];
+        }
+        for(int i=nums.length-1;i>=0;i--){
+            ans[i]=ans[i]*suffix;
+            suffix=suffix*nums[i];
+        }
+        System.out.print("[");
+        for(int i=0;i<ans.length;i++){
+            System.out.print(ans[i]+" ");
+        }
+        System.out.print("]");
+        return ans;
+    }
+    public static void main(String[] args) {
+        int[] nums={1,2,3,4};
+        ProductofArrayExceptSelf product=new ProductofArrayExceptSelf();
+        product.prductofarray(nums);
+    }
+}
